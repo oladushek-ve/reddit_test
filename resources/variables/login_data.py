99d28b62641a783311fb2f99ca8ac913dcbd0dd9
Vadim Yhlin <vyhlin@script-master.net>
2022-08-17 18:04:44 +0700

    Added datas for authorization

--- resources/variables/login_data.py
+++ resources/variables/login_data.py
@@ -1,4 +1,6 @@
 import os
 
-username_reddit = os.getenv('LOGIN_REDDIT')
-password_reddit = os.getenv('PASSWORD_REDDIT')
+username = os.getenv('LOGIN_REDDIT')
+password = os.getenv('PASSWORD_REDDIT')
+client_id = os.getenv('CLIENT_REDDIT_ID')
+secret_key = os.getenv('SECRET_REDDIT_KEY')
