62b7d8e38a71ca19a71ba88a40194890897af950
Vadim Yhlin <vyhlin@script-master.net>
2022-08-18 11:33:43 +0700

    added new urls

--- resources/variables/conf.py
+++ resources/variables/conf.py
@@ -1,2 +1,9 @@
 token_url = 'https://ssl.reddit.com/'
+base_url = 'https://oauth.reddit.com/'
+
 useragent = 'MyAPI/0.0.1'
+url_search = 'search/?q='
+url_send_post = 'api/comment'
+url_del_comment = 'api/del'
+auth_url = 'api/v1/access_token'
+check_auth_url = 'api/v1/me'
